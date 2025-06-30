6d4b0d63e1c70c161c397ea8b12744a1ae581391
“Nina <2917069w@student.gla.ac.uk>
2025-06-30 16:10:30 +0100

    Wrote TextEntry class, deleted Tag class

--- notesapp_parsing_project/src/main/java/Tag.java
+++
@@ -1,1 +0,0 @@
-package notesapp_parsing_project;
--- notesapp_parsing_project/src/main/java/TextEntry.java
+++ notesapp_parsing_project/src/main/java/TextEntry.java
@@ -1,1 +1,38 @@
 package notesapp_parsing_project;
+
+public class TextEntry{
+    protected String content;
+    protected String tag;
+
+    public TextEntry(String content, String tag){
+        this.content = content;
+        this.tag = tag;
+    }
+
+    public TextEntry(String content){
+        this.content = content;
+        this.tag = null;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getTag() {
+        if (this.tag == null){
+            return "null"
+        }
+        return this.tag;
+    }
+
+    @Override
+    public String toString(){
+        return ("TextEntry{" +
+               "content='" + content + '\'' +
+               ", tag='" + tag + '\'' +
+               '}');
+    }
+
+    
+
+}
