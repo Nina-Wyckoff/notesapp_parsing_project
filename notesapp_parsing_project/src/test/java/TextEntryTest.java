043102e64c67c8414cfaa70ca05b59c8477803d0
“Nina <2917069w@student.gla.ac.uk>
2025-06-30 16:18:50 +0100

    Planned TextEntry class unit tests

--- notesapp_parsing_project/src/test/java/TextEntryTest.java
+++ notesapp_parsing_project/src/test/java/TextEntryTest.java
@@ -1,5 +1,39 @@
 package notesapp_parsing_project;
 
 import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TextEntryTest {
+
+    @Test
+    void testTextEntryCreationTagged(){
+
+    }
+
+    @Test
+    void testTextEntryCreationUntagged(){
+
+    }
+
+    @Test
+    void testGetContent(){
+
+    }
+
+    @Test 
+    void testGetTagUntagged(){
+
+    }
+
+    @Test
+    void testGetTagTagged(){
+
+    }
+
+    @Test
+    void testToString(){
+        
+    }
+}
