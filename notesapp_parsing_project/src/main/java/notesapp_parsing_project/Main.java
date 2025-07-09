06287e50e048fe9624a8faa4ea4da00315869851
“Nina <2917069w@student.gla.ac.uk>
2025-07-09 18:57:16 +0100

    Added project package directory into structure

--- notesapp_parsing_project/src/main/java/notesapp_parsing_project/Main.java
+++ notesapp_parsing_project/src/main/java/notesapp_parsing_project/Main.java
@@ -4,10 +4,11 @@ import notesapp_parsing_project.MDParserStrategy;
 import notesapp_parsing_project.SingleLineSegmentStrategy;
 import notesapp_parsing_project.UnspacedSegmentStrategy;
 
-public class App 
+public class Main 
 {
     public static void main( String[] args )
     {
         MDParserStrategy parser = new MDParserStrategy(SingleLineSegmentStrategy);
+        parser.parse("/Users/ninawyckoff/Desktop/Notes_App_Archive_Jun_2025/iCloud/Notes/Names I like.md");
     }
 }
